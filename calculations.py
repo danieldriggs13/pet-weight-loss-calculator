79e9a8b4dd552d6565385a528b3ec7c136f13626
Daniel Driggs <danieldriggs13@gmail.com>
2026-08-19 16:27:50 -0600

    Add project structure and skeleton files

---
+++ calculations.py
@@ -0,0 +1,19 @@
+def get_species():
+    #TODO: Return the selected species (dog or cat)
+    pass
+
+def get_weight():
+    #TODO: Return the current weight (in lbs)
+    pass
+
+def get_bcs():
+    #TODO: Return the body condition score (out of 9)
+    pass
+
+def calculate_ideal_weight(species, current_weight, bcs):
+    #TODO: Calculate ideal weight based on species, current weight, and BCS
+    pass
+
+def calculate_daily_calories(ideal_weight, species):
+    #TODO: Calculate recommended daily calorie range for the ideal weight
+    pass
