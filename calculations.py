c8967098566909a7873eb56ebb9195c9a81403f4
Daniel Driggs <danieldriggs13@gmail.com>
2026-08-19 22:02:49 -0600

    Add input handling and validation

--- calculations.py
+++ calculations.py
@@ -1,25 +1,50 @@
 def get_species():
+    """Get and return the pet's species via console input. Returns a string - "dog" or "cat"."""
     while True:
         species = input('Type "d" for dog, "c" for cat: ').lower()
 
         if species in ("d", "c"):
             species_name = "dog" if species == "d" else "cat"
             print(f"You selected {species_name}")
-            return species
+            return species_name
 
         print("Invalid input, please try again.\n")
 
+
 def get_weight():
-    #TODO: Return the current weight (in lbs)
-    pass
+    """Get and return the pet's current weight in lbs via console input. Returns a float."""
+    while True:
+        try:
+            weight = float(input('Enter weight in lbs: '))
+
+            if 0 < weight < 300:
+                return weight
+
+            print("Invalid input: Impossible weight! Please try again.")
+
+        except ValueError:
+            print("That is not a valid number! Weight must be a whole number or decimal.\nPlease try again.")
+
 
 def get_bcs():
-    #TODO: Return the body condition score (out of 9)
-    pass
+    """Get and return the pet's Body Condition Score (1-9, whole number) via console input. Returns an integer."""
+    while True:
+        try:
+            bcs = int(input('Enter Body Condition Score (whole number 1-9): '))
+
+            if 0 < bcs <= 9:
+                return bcs
+
+            print("Invalid input! BCS must be a whole number between 1 and 9.\nPlease try again.")
+
+        except ValueError:
+            print("That is not a valid number! Please try again.")
+
 
 def calculate_ideal_weight(species, current_weight, bcs):
     #TODO: Calculate ideal weight based on species, current weight, and BCS
     pass
+
 
 def calculate_daily_calories(ideal_weight, species):
     #TODO: Calculate recommended daily calorie range for the ideal weight
--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import calculations
 
 def main():
     species = calculations.get_species()
-    print(species)
+    print(species)  # test
+
+    weight = calculations.get_weight()
+    print(weight) # test
+
+    bcs = calculations.get_bcs()
+    print(bcs) # test
 
 if __name__ == "__main__":
     main()
